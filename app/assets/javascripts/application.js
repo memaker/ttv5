2ed61ffdfd806d7f1bcf80845745deade99441e7
Roberto Bartolome <roberto.bartolome.medina@gmail.com>
2013-10-30 23:06:56 +0100

    Working Bootswatch gem version

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-require jquery
-require jquery_ujs
-require twitter/bootstrap
-require_tree .
-require_tree flot
-require_tree fuelux
-require_tree markdown
+//= require jquery
+//= require jquery_ujs
+//= require twitter/bootstrap
+//= require united/loader
+//= require united/bootswatch
+
+
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,7 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
-
+  
+  # from https://github.com/scottvrosenthal/twitter-bootswatch-rails
+  layout 'united'
 end
